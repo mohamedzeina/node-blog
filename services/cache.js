c836b2f94ad7369be5bec65dbd38c3b440854554
Mohamed Zeina <toxiczeina@gmail.com>
2025-09-14 15:59:42 +0300

    generated unique caching keys

--- services/cache.js
+++ services/cache.js
@@ -3,7 +3,9 @@ const mongoose = require('mongoose');
 const exec = mongoose.Query.prototype.exec; // Save reference to original exec function
 
 mongoose.Query.prototype.exec = function () {
-	console.log('Qeury will be executed');
+	const key = Object.assign({}, this.getQuery(), {
+		collection: this.mongooseCollection.name,
+	}); // Create a unique key for the query
 
 	return exec.apply(this, arguments); // Call original exec function with original arguments
 };
