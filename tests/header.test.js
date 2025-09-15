10a886351b4764b25b2b20c0b3a150c94648f6a3
Mohamed Zeina <toxiczeina@gmail.com>
2025-09-16 01:58:59 +0300

    Update header.test.js to navigate to app and verify main page

--- tests/header.test.js
+++ tests/header.test.js
@@ -5,9 +5,13 @@ test('Adds two numbers', () => {
 	expect(sum).toEqual(3);
 });
 
-test('We can launch a browser', async () => {
+test('We can launch a browser, navigate to app', async () => {
 	const browser = await puppeteer.launch({
 		headless: true,
 	});
 	const page = await browser.newPage();
+	await page.goto('http://localhost:3000');
+
+	const text = await page.$eval('a.brand-logo', (el) => el.innerHTML);
+	expect(text).toEqual('Blogster');
 });
