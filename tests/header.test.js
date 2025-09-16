e7e29837f119bb3ebf8cc3e1c275f14442b2fe25
Mohamed Zeina <toxiczeina@gmail.com>
2025-09-16 18:07:07 +0300

    Refactor test setup: move browser/page initialization to beforeEach hook

--- tests/header.test.js
+++ tests/header.test.js
@@ -1,17 +1,16 @@
 const puppeteer = require('puppeteer');
 
-test('Adds two numbers', () => {
-	const sum = 1 + 2;
-	expect(sum).toEqual(3);
+let browser, page;
+
+beforeEach(async () => {
+  browser = await puppeteer.launch({
+    headless: false,
+  });
+  page = await browser.newPage();
+  await page.goto('http://localhost:3000');
 });
 
 test('We can launch a browser, navigate to app', async () => {
-	const browser = await puppeteer.launch({
-		headless: true,
-	});
-	const page = await browser.newPage();
-	await page.goto('http://localhost:3000');
-
-	const text = await page.$eval('a.brand-logo', (el) => el.innerHTML);
-	expect(text).toEqual('Blogster');
+  const text = await page.$eval('a.brand-logo', (el) => el.innerHTML);
+  expect(text).toEqual('Blogster');
 });
