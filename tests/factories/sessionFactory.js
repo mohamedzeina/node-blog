5fa7632abc20094c2d3a07f4be6c53ea570ea997
Mohamed Zeina <toxiczeina@gmail.com>
2025-10-06 17:25:58 +0300

    Refactor session generating code into a session factory

---
+++ tests/factories/sessionFactory.js
@@ -0,0 +1,18 @@
+const Keygrip = require('keygrip');
+const keys = require('../../config/keys');
+const { Buffer } = require('buffer');
+const keygrip = new Keygrip([keys.cookieKey]);
+
+module.exports = (user) => {
+  const sessionObject = {
+    passport: {
+      user: user._id.toString(),
+    },
+  };
+
+  const session = Buffer.from(JSON.stringify(sessionObject)).toString('base64');
+
+  const sig = keygrip.sign('session=' + session);
+
+  return { session, sig };
+};
