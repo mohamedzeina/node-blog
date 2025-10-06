b6fd34aef69596f531699fda6750a97d6fe6ac65
Mohamed Zeina <toxiczeina@gmail.com>
2025-10-06 17:26:47 +0300

    Finish refactor

--- tests/header.test.js
+++ tests/header.test.js
@@ -30,22 +30,6 @@ test('Clicking login starts OAuth flow', async () => {
 test('When signed in, shows logout button', async () => {
   const id = '68c054fdd44c9242a5bbbd40';
 
-  const { Buffer } = require('buffer');
-  const sessionObject = {
-    passport: {
-      user: id,
-    },
-  };
-
-  const sessionString = Buffer.from(JSON.stringify(sessionObject)).toString(
-    'base64'
-  );
-
-  const Keygrip = require('keygrip');
-  const keys = require('../config/keys');
-  const keygrip = new Keygrip([keys.cookieKey]);
-  const sig = keygrip.sign('session=' + sessionString);
-
   await page.setCookie({ name: 'session', value: sessionString });
   await page.setCookie({ name: 'session.sig', value: sig });
 
