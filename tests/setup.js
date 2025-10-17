5594e03ea875990103a68ae75f427fecfc5667b6
Mohamed Zeina <toxiczeina@gmail.com>
2025-10-17 14:55:33 +0300

    Add page.js in tests/helpers for proxy setup for custom pages

--- tests/setup.js
+++ tests/setup.js
@@ -1,7 +1,14 @@
 require('../models/User');
 
-const keys = require('../config/keys');
 const mongoose = require('mongoose');
+const keys = require('../config/keys');
 
 mongoose.Promise = global.Promise;
-mongoose.connect(keys.mongoURI, { useMongoClient: True });
+mongoose.connect(keys.mongoURI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+afterAll(async () => {
+  await mongoose.disconnect();
+});
