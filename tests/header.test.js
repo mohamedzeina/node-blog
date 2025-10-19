b42e7017c628c289b14d28afbf0f4d2b44303146
Mohamed Zeina <toxiczeina@gmail.com>
2025-10-19 18:07:36 +0300

    Finish custom page implementation

--- tests/header.test.js
+++ tests/header.test.js
@@ -1,52 +1,43 @@
-const puppeteer = require('puppeteer');
 const sessionFactory = require('./factories/sessionFactory');
 const userFactory = require('./factories/userFactory');
+const Page = require('./helpers/page');
 
-let browser, page;
+let page;
 
 beforeEach(async () => {
-	browser = await puppeteer.launch({
-		headless: true,
-	});
-	page = await browser.newPage();
-	await page.goto('http://localhost:3000');
+  page = await Page.build();
+  await page.goto('http://localhost:3000');
 });
 
 afterEach(async () => {
-	await browser.close();
+  await page.close();
 });
 
 test('The header has the correct text', async () => {
-	const text = await page.$eval('a.brand-logo', (el) => el.innerHTML);
-	expect(text).toEqual('Blogster');
+  const text = await page.$eval('a.brand-logo', (el) => el.innerHTML);
+  expect(text).toEqual('Blogster');
 });
 
 test('Clicking login starts OAuth flow', async () => {
-	await page.click('.right a');
+  await page.click('.right a');
 
-	const url = await page.url();
+  const url = await page.url();
 
-	expect(url).toMatch(/accounts\.google\.com/);
+  expect(url).toMatch(/accounts\.google\.com/);
 });
 
 test('When signed in, shows logout button', async () => {
-<<<<<<< HEAD
   const user = await userFactory();
   const { session, sig } = sessionFactory(user);
-=======
-	const user = await userFactory();
->>>>>>> 1c1bfb23149633364e87db9860d1fb7ae771516f
 
-	const { session, sig } = sessionFactory(user);
+  await page.setCookie({ name: 'session', value: session });
+  await page.setCookie({ name: 'session.sig', value: sig });
 
-	await page.setCookie({ name: 'session', value: session });
-	await page.setCookie({ name: 'session.sig', value: sig });
+  await page.goto('http://localhost:3000'); // Refresh the page to simulate logging in
+  await page.waitFor('a[href="/auth/logout"]'); // Wait for the logout button to appear
 
-	await page.goto('http://localhost:3000'); // Refresh the page to simulate logging in
-	await page.waitFor('a[href="/auth/logout"]'); // Wait for the logout button to appear
+  const text = await page.$eval('a[href="/auth/logout"]', (el) => el.innerHTML);
+  console.log('Logout button text:', text); // Debugging line
 
-	const text = await page.$eval('a[href="/auth/logout"]', (el) => el.innerHTML);
-	console.log('Logout button text:', text); // Debugging line
-
-	expect(text).toEqual('Logout');
+  expect(text).toEqual('Logout');
 });
--- tests/setup.js
+++ tests/setup.js
@@ -5,19 +5,10 @@ const keys = require('../config/keys');
 
 mongoose.Promise = global.Promise;
 mongoose.connect(keys.mongoURI, {
-<<<<<<< HEAD
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
 
 afterAll(async () => {
   await mongoose.disconnect();
-=======
-	useNewUrlParser: true,
-	useUnifiedTopology: true,
-});
-
-afterAll(async () => {
-	await mongoose.disconnect();
->>>>>>> 1c1bfb23149633364e87db9860d1fb7ae771516f
 });
