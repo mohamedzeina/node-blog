e714456f698e28edf9dbedc74e5aebabe92e1093
Mohamed Zeina <toxiczeina@gmail.com>
2026-03-01 16:59:58 +0300

    Add route handler for getting presigned url using Amazon S3for image upload

---
+++ routes/uploadRoutes.js
@@ -0,0 +1,32 @@
+const AWS = require('aws-sdk');
+const uuid = require('uuid/v1');
+const requireLogin = require('../middlewares/requireLogin');
+const keys = require('../config/keys');
+
+const s3 = new AWS.S3({
+	credentials: {
+		accessKeyId: keys.accessKeyId,
+		secretAccessKey: keys.secretAccessKey,
+	},
+	region: 'eu-central-1',
+});
+
+module.exports = (app) => {
+	app.get('/api/upload', requireLogin, (req, res) => {
+		// Handle image upload logic here
+
+		const key = `${req.user.id}/${uuid()}.jpeg`;
+
+		s3.getSignedUrl(
+			'putObject',
+			{
+				Bucket: 'adv-nodejs-blog-bucket',
+				ContentType: 'jpeg',
+				Key: key,
+			},
+			(err, url) => {
+				res.send({ key, url });
+			},
+		);
+	});
+};
